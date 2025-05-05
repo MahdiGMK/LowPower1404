fdc870f52c51f77cb237c425c54c9223129e772a
MahdiGMK <mbahramiand@gmail.com>
2025-05-05 23:29:59 +0330

    midterm 4,5

--- MidTerm/Q3/digital/NormalAdder.v
+++ MidTerm/Q3/digital/NormalAdder.v
@@ -1,4 +1,4 @@
-module MCCAdder (
+module NormalAdder (
     input  [15:0] a,
     input  [15:0] b,
     output [16:0] s
@@ -7,8 +7,8 @@ module MCCAdder (
     assign C[-1] = 0;
     genvar i;
     generate
-        for (i = 0; i < 4; i++) begin : gen_mcc
-            MCC4 _mcc (
+        for (i = 0; i < 4; i++) begin : gen_fa
+            FA4 _fa (
                 .a(a[i*4+:4]),
                 .b(b[i*4+:4]),
                 .cin(C[i-1]),
@@ -20,69 +20,72 @@ module MCCAdder (
     assign s[16] = C[3];
 endmodule
 
-module MCC4 (
+module FA4 (
     input [3:0] a,
     input [3:0] b,
     input cin,
     output [3:0] s,
     output cout
 );
-    wire [3:-1] C;
-    assign C[-1] = ~cin;
-    genvar i;
-    generate
-        for (i = 0; i < 4; i += 1) begin : gen_mcc
-            MCC1 _1 (
-                .a(a[i]),
-                .b(b[i]),
-                ._cin(C[i-1]),
-                ._cout(C[i]),
-                .s(s[i])
-            );
-        end
-    endgenerate
-    assign cout = ~C[3];
+    wire [2:0] cinter;
+
+    FullAdder _0 (
+        .a(a[0]),
+        .b(b[0]),
+        .c(cin),
+        .cout(cinter[0]),
+        .s(s[0])
+    );
+
+    FullAdder _1 (
+        .a(a[1]),
+        .b(b[1]),
+        .c(cinter[0]),
+        .cout(cinter[1]),
+        .s(s[1])
+    );
+
+    FullAdder _2 (
+        .a(a[2]),
+        .b(b[2]),
+        .c(cinter[1]),
+        .cout(cinter[2]),
+        .s(s[2])
+    );
+
+    FullAdder _3 (
+        .a(a[3]),
+        .b(b[3]),
+        .c(cinter[2]),
+        .cout(cout),
+        .s(s[3])
+    );
 endmodule
 
-module MCC1 (
+module FullAdder (
     input  a,
     input  b,
-    input  _cin,
+    input  c,
     output s,
-    output _cout
+    output cout
 );
-    wire P;
-    wire G;
-    xor (P, a, b);
-    and (G, a, b);
-    wire _wk1;
-    wire _st0;
-    assign (pull1, weak0) _wk1 = 1;
-    // assign _wk1 = 1;
-    assign _st0 = 0;
-    pmos _p0 (_cout, _wk1, G);
-    wire _inter;
-    nmos _n0 (_cout, _inter, G);
-    nmos _n1 (_inter, _st0, ~P);
-    nmos _n2 (_cout, _cin, P);
-    xnor (s, P, _cin);
+    assign {cout, s} = a + b + c;
 endmodule
 
-// module mcc4test;
-//     reg [3:0] a, b;
+// module fa4test;
 //     reg cin;
-//     wire [3:0] s;
+//     reg [3:0] a, b;
 //     wire cout;
-//     MCC4 _mcc (
+//     wire [3:0] s;
+//     FA4 _fa (
 //         .a(a),
 //         .b(b),
 //         .cin(cin),
-//         .cout(cout),
-//         .s(s)
+//         .s(s),
+//         .cout(cout)
 //     );
 //     int i, j, k;
 //     initial begin
-//         // $monitor("%b %b %b => %b %b", a, b, cin, cout, s);
 //         for (i = 0; i < 16; i += 1)
 //         for (j = 0; j < 16; j += 1)
 //         for (k = 0; k < 2; k += 1) begin
@@ -95,10 +98,34 @@ endmodule
 //     end
 // endmodule
 
-// module mcctest;
+// module fatest;
+//     reg a, b, c;
+//     wire s, cout;
+//     FullAdder _fa (
+//         .a(a),
+//         .b(b),
+//         .c(c),
+//         .s(s),
+//         .cout(cout)
+//     );
+//     initial begin
+//         $monitor(a, b, c, " => ", cout, s);
+//         {a, b, c} = #1 0;
+//         {a, b, c} = #1 1;
+//         {a, b, c} = #1 2;
+//         {a, b, c} = #1 3;
+//         {a, b, c} = #1 4;
+//         {a, b, c} = #1 5;
+//         {a, b, c} = #1 6;
+//         {a, b, c} = #1 7;
+//     end
+// endmodule
+
+
+// module addrtest;
 //     reg [15:0] a, b;
 //     wire [16:0] s;
-//     MCCAdder _mcc (
+//     NormalAdder _addr (
 //         .a(a),
 //         .b(b),
 //         .s(s)
